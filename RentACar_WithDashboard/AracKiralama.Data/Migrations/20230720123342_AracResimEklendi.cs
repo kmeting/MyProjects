fb1d0a9b941dcd13fd6a4d6a6cbdab8de2430d7d
kmeting <139559498+kmeting@users.noreply.github.com>
2023-07-25 15:30:54 +0300

    RentACar_WithDashboard

---
+++ RentACar_WithDashboard/AracKiralama.Data/Migrations/20230720123342_AracResimEklendi.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AracKiralama.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AracResimEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Resim1",
+                table: "Araclar",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resim2",
+                table: "Araclar",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resim3",
+                table: "Araclar",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 7, 20, 15, 33, 42, 647, DateTimeKind.Local).AddTicks(7681));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Resim1",
+                table: "Araclar");
+
+            migrationBuilder.DropColumn(
+                name: "Resim2",
+                table: "Araclar");
+
+            migrationBuilder.DropColumn(
+                name: "Resim3",
+                table: "Araclar");
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 7, 6, 11, 14, 51, 827, DateTimeKind.Local).AddTicks(1876));
+        }
+    }
+}
---
+++ RentACar_WithDashboard/AracKiralama.WebUI/Areas/Admin/Models/HomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AracKiralama.Entities;
+
+namespace AracKiralama.WebUI.Areas.Admin.Models
+{
+    public class HomePageViewModel
+    {
+        public  List<Slider> Sliders { get; set; }
+        public List<Arac> Araclar { get; set; }
+    }
+}
