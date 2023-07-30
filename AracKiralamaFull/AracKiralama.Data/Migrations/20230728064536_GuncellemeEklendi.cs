94e9143afe3df9e6230f0834f16287de88fa9a20
kmeting <139559498+kmeting@users.noreply.github.com>
2023-07-30 15:23:49 +0300

    RentACarFull

---
+++ AracKiralamaFull/AracKiralama.Data/Migrations/20230728064536_GuncellemeEklendi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AracKiralama.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuncellemeEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 7, 28, 9, 45, 36, 294, DateTimeKind.Local).AddTicks(3513));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 7, 26, 11, 21, 17, 780, DateTimeKind.Local).AddTicks(2010));
+        }
+    }
+}
